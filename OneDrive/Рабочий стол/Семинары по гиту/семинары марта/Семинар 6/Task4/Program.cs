36b8dcb1f67b281513cddda8bb87fa610c45dcb1
elena <boliasova64@mail.ru>
2022-03-27 18:53:39 +0900

    Task4

---
+++ OneDrive/Рабочий стол/Семинары по гиту/семинары марта/Семинар 6/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+int[] arr = RandArray(20);
+PrintArray(arr);
+int[] arr2 = RandArray(arr.Length);
+for(int i = 0; i < arr.Length; i++)
+{
+arr2[i] = arr[i];
+}
+PrintArray(arr2);
+void PrintArray(int[] Mas)
+{
+    int count = Mas.Length;
+    for (int pos = 0; pos < count; pos++)
+    {
+        Console.Write(Mas[pos] + " ");
+    }
+    Console.WriteLine();   
+}
+int[] RandArray(int lenght)
+{
+    int[] answer = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        answer[i] = new Random().Next();
+    }
+    return answer;
+}
+
+
