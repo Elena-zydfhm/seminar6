e76dcacc23d2fcb147f67643941fd1744639cea1
elena <boliasova64@mail.ru>
2022-03-27 20:02:50 +0900

    Task4

---
+++ OneDrive/Рабочий стол/Семинары по гиту/семинары марта/Семинар 6/HW1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int[] a = new int[10];
+Console.WriteLine("Введите числа: =>  ");
+int summ=0; 
+for (int i = 0; i < a.Length; i++) 
+{
+    if (Int32.TryParse(Console.ReadLine(), out a[i]) != true) 
+    {
+        Console.Write("Enter correct naumber");
+    }
+    else 
+    {
+        if (a[i] > 0)
+        {
+           summ++;
+        }
+    }
+}
+Console.WriteLine($"Количество положительных чисел, введённых пользователем {summ}");
