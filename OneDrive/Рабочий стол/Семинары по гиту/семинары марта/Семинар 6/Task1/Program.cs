c4d49dca3d14dd328204dede4e09ceebe18210e8
elena <boliasova64@mail.ru>
2022-03-27 18:06:29 +0900

    Task1

---
+++ OneDrive/Рабочий стол/Семинары по гиту/семинары марта/Семинар 6/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход три числа и проверяет, 
+//может ли существовать треугольник с сторонами такой длины.
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+int a = Prompt("Введите число A => ");
+int b = Prompt("Введите число В => ");
+int c = Prompt("Введите число С => ");
+if(a+b > c && a+c > b && b+c > a)
+Console.WriteLine("Треугольник существует" );
+else Console.WriteLine("Треугольник не существует" );
